10fc6320afa85d6b3d72c2ddcc7725151f0b4934
40230153 <46711038+cmctaggart03id40230153@users.noreply.github.com>
2021-10-19 11:39:27 +0100

    kbnsdvb

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 	public int add(int a, int b) {
 		return a+b+1;
 	}
-	//adding for commit
+	//adding for commit i hate u travis
 	public int sub(int a, int b) {
 		return a-b;
 	} 
